8deb44b7c69fb18213acfedddba1378e6f650311
sabbir-anwar <protik26@gmail.com>
2020-12-21 17:29:49 +0600

    area model

---
+++ models/areaModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const areaSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("Area", areaSchema)
